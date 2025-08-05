e48b480b882add4565a4ff0c92d2a56bd909add2
Amir Mohammadi <amirja651@gmail.com>
2025-08-05 09:45:55 -0400

    6.3.0.5.1

--- src/Pins.cpp
+++ src/Pins.cpp
@@ -1,7 +1,11 @@
 #include "Pins.h"
 
-const uint16_t DriverPins::DIR[4]     = {22, 4, 32, 27};
-const uint16_t DriverPins::STEP[4]    = {21, 16, 33, 14};
-const uint16_t DriverPins::EN[4]      = {17, 15, 26, 13};
+const uint16_t DriverPins::DIR[4] = {22, 4, 32, 27};  // blue
+
+const uint16_t DriverPins::STEP[4] = {21, 16, 33, 14};  // red
+
+const uint16_t DriverPins::CS[4] = {5, 2, 25, 12};  // green
+
+const uint16_t DriverPins::EN[4] = {17, 15, 26, 13};  // black
+
 const uint16_t EncoderPins::SIGNAL[4] = {36, 39, 34, 35};
-const uint16_t DriverPins::CS[4]      = {5, 2, 25, 12};
